b1fed2a56ea2acfb28cb16d3c80fbe603dea2acf
Lasabi Olalekan <84998804+Larshabi@users.noreply.github.com>
2022-10-10 22:51:02 +0100

    secret

--- gidifusion/settings.py
+++ gidifusion/settings.py
@@ -10,13 +10,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-it##=)15yu9jhe^ozq*g+gvmywhr$$w0ko*+c-i@&)m%rfer2c'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
