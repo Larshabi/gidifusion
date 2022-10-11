78a218058dfca232478c415dc7b4d482f761ee1e
Lasabi Olalekan <84998804+Larshabi@users.noreply.github.com>
2022-10-11 02:06:15 +0100

    cors

--- gidifusion/settings.py
+++ gidifusion/settings.py
@@ -30,10 +30,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'team',
     'rest_framework',
-    'drf_yasg'
+    'drf_yasg',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
